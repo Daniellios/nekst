03e562e52116b029c88d19181ced1dbc97bb76d1
Daniellios <danielo49134@gmail.com>
2022-12-27 20:32:54 +0300

    color adjustment

--- src/components/ProductCardReview.tsx
+++ src/components/ProductCardReview.tsx
@@ -15,17 +15,22 @@ const ProductCardReview: FC<IProductCardReview> = ({ rate, category }) => {
   return (
     <div className={styles.product__ratings}>
       <p className={styles.product__category}>{category}</p>
-      <div className={styles.product__reviews}>
-        <div>
-          {calculateRating(rate).map((isRed: boolean, idx: number) => {
-            return <RatingStar key={idx} isRed={isRed}></RatingStar>;
-          })}
-        </div>
 
-        <Link href={"/"} className={styles.product__reviews_amount}>
-          {calculateReviews(rate)}
-        </Link>
-      </div>
+      {rate ? (
+        <div className={styles.product__reviews}>
+          <div>
+            {calculateRating(rate).map((isRed: boolean, idx: number) => {
+              return <RatingStar key={idx} isRed={isRed}></RatingStar>;
+            })}
+          </div>
+
+          <Link href={"/"} className={styles.product__reviews_amount}>
+            {calculateReviews(rate)}
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.product__reviews}> </div>
+      )}
     </div>
   );
 };
--- src/components/ProductCartManager.tsx
+++ src/components/ProductCartManager.tsx
@@ -68,13 +68,7 @@ const ProductCartManager: FC<IProductCartManager> = ({ productID }) => {
               className={styles.product__btn_amount}
               onClick={increaseProductAmount}
             >
-              <svg
-                width="10"
-                height="10"
-                viewBox="0 0 10 10"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+              <svg width="10" height="10" viewBox="0 0 10 10" fill="none">
                 <g opacity="0.3">
                   <rect y="4" width="10" height="2" fill="#2E2E33" />
                   <rect
